20b35e052f53ff0b764c3d5c55ba552527458c4d
SokCheasrouch <cheasrouch.sok@gmail.com>
2024-09-02 14:52:43 +0700

    modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { CustomExceptionFilter, ResponseInterceptor } from '@app/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new CustomExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(3000);
 }
+
 bootstrap();
